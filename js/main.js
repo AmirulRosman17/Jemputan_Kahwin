f9c42bfcb99e5143491454becf69edc5239e31e5
AmirulRosman17 <150363397+AmirulRosman17@users.noreply.github.com>
2026-04-18 13:07:15 +0800

    Update event start and end times

--- js/main.js
+++ js/main.js
@@ -108,8 +108,8 @@ setupCountdown(".campaign-0", new Date().getTime(), 1787369400000);
   ======================================================= */
 const event = {
     title: "Jemputan Kenduri Kahwin Putri & Amirul",
-    startDate: "20260822T030000Z", // YYYYMMDDTHHmmssZ (UTC)
-    endDate: "20260822T080000Z",
+    startDate: "20260822T033000Z", // YYYYMMDDTHHmmssZ (UTC)
+    endDate: "20260822T083000Z",
     location: "Bayu Bay Hall, Jalan Balik Bukit",
     description: "Kami menjemput tuan/puan hadir ke majlis perkahwinan anakanda kami.",
 };
